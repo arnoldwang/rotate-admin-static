41479929544e563b0b7e574c114f2c0249e3b6a9
yangjie <hiyoungjay@gmail.com>
2015-01-20 17:13:22 +0800

    init

--- app/index.js
+++ app/index.js
@@ -2,15 +2,15 @@ var _ = require('lodash');
 var ko = require('knockout');
 var mixin = require('mixin-class');
 
-var PageBuilder = require('crystal-page');
+var Page = require('crystal-page');
 var State = require('crystal-state');
 require('./setup-knockout');
 
-var pageBuilder = new PageBuilder();
-var state = new State.Hash();
+var page = new Page();
+var state = new State[ENV.mock ? 'Hash': 'Location']();
 
 state.onChange(function(data) {
-    pageBuilder.activate(data.path, function(module) {
+    page.activate(data.path, function(module) {
         if (module.onStateChange) {
             module.onStateChange(data.query);
         }
@@ -20,13 +20,20 @@ state.onChange(function(data) {
 
 // 所有的module都会继承这个类，写在gulpfile里面
 var ViewModel = mixin({
-    $state: state
+    getAppState: function() {
+        return state.getData();
+    },
+
+    setAppState: function(data) {
+        state.setData(data)
+    }
 });
 
 ViewModel.create = function(props) {
     return _.extend(new this, props);
 };
 
+// gulpfile里面用到
 window.vm = function(mixins, html) {
     var vm = ViewModel.extend(mixins);
     if (html) {
@@ -37,9 +44,8 @@ window.vm = function(mixins, html) {
     return vm;
 };
 
-
 // start app
 state.start();
-pageBuilder.render(document.body);
+page.render(document.body);
 
 
