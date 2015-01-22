826804e5c342942aa028a0a601e823599eb1294f
yangjie <hiyoungjay@gmail.com>
2015-01-22 11:54:05 +0800

    add

--- server-config.js
+++ server-config.js
@@ -1,23 +1,7 @@
 var _ = require('lodash');
 
 module.exports = {
-    "/territory/children": {
-        "method": "get",
-        "params": {
-            "id": 1
-        },
-        "response": [
-            {
-                "id": 1,
-                "name": "上海"
-            },
-            {
-                "id": 2,
-                "name": "北京"
-            }
-        ]
-    },
-    "/territory/descendants": {
+    "/data/territory/tree": {
         "method": "get",
         "params": {
             "id": 1
@@ -25,62 +9,65 @@ module.exports = {
         "response": 
         // function() {
         //     var deep = 3;
-        //     var children = 3;
+        //     var childrenList = 3;
 
         //     var createLevel = function(level) {
         //         if (level === deep) {
         //             return [];
         //         }
 
-        //         return _.times(children, function(i) {
+        //         return _.times(childrenList, function(i) {
         //             return {
         //                 id: level * deep + i,
         //                 name: 'level=' + level + ',i=' + i,
-        //                 children: createLevel(level + 1)
+        //                 childrenList: createLevel(level + 1)
         //             }
         //         })
         //     };
 
         //     return createLevel(0);
         // }
-        [
-            {
-                "id": 1,
-                "name": "上海",
-                "children": [
-                    {
-                        "id": 3,
-                        "name": "长宁",
-                        "children": [
-                            {
-                                "id": 33,
-                                "name": "中山公园"
-                            }
-                        ]
-                    },
-                    {
-                        "id": 4,
-                        "name": "浦东"
-                    }
-                ]
-            },
-            {
-                "id": 2,
-                "name": "北京",
-                "children": [
-                    {
-                        "name": "朝阳",
-                        "children": [
-                            {
-                                "name": "东直门"
-                            },
-                            {
-                                "name": "东弯门"
-                            }
-                        ]
-                    }
-                ]
-            }
-        ]
+        {
+            "territoryName": "点评",
+            "childrenList": [
+                {
+                    "id": 1,
+                    "territoryName": "上海",
+                    "childrenList": [
+                        {
+                            "id": 3,
+                            "territoryName": "长宁",
+                            "childrenList": [
+                                {
+                                    "id": 33,
+                                    "territoryName": "中山公园"
+                                }
+                            ]
+                        },
+                        {
+                            "id": 4,
+                            "territoryName": "浦东"
+                        }
+                    ]
+                },
+                {
+                    "id": 2,
+                    "territoryName": "北京",
+                    "childrenList": [
+                        {
+                            "territoryName": "朝阳",
+                            "childrenList": [
+                                {
+                                    "territoryName": "东直门"
+                                },
+                                {
+                                    "territoryName": "东弯门"
+                                }
+                            ]
+                        }
+                    ]
+                }
+            ]
+        }        
     }
 }
--- server.js
+++ server.js
@@ -9,7 +9,6 @@ _.forEach(require('./server-config.js'), function(config, path) {
     if (!config.status) {
         config.status = 200;
     }
-
     app[config.method || 'get'](path, function(req, res) {
         res.status(config.status).send(typeof config.response === 'function' ? config.response(req, res) : config.response);
     })
